64b39ae0cf9dd091dd63e3bcc96e46e5b47d5c46
Lopezs20 <lopezs20@outlook.com>
2022-08-06 15:10:45 -0700

    Initialized Maven Archetype

---
+++ source/src/main/java/OOP_practice/App.java
@@ -0,0 +1,13 @@
+package OOP_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
